50ecd241b89bb5f60d893978d9e8f29f74f7144b
Gustavo Carreno <guscarreno@gmail.com>
2018-02-02 15:51:19 +0000

    Added more GUI: Path, Status, Error, Log, Clear Logs.

--- UberPDFUpdate.Forms.Main.pas
+++ UberPDFUpdate.Forms.Main.pas
@@ -6,24 +6,56 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, JSONPropStorage, Menus,
-  ActnList, StdActns, LCLType;
+  ActnList, StdActns, LCLType, ExtCtrls, StdCtrls, PairSplitter, SynEdit;
 
 type
 
   { TfrmMain }
 
   TfrmMain = class(TForm)
+    actFileUpdate: TAction;
+    actHelpAbout: TAction;
+    actLoggingClearLogs: TAction;
     alMain: TActionList;
     actFileExit: TFileExit;
+    btnFileUpdate: TButton;
+    btnClearLogs: TButton;
+    cbAutoClear: TCheckBox;
+    edtStatus: TEdit;
+    edtPath: TEdit;
+    lblPathTitle: TLabel;
+    memError: TMemo;
+    mnuHelpAbout: TMenuItem;
+    mnuLoggingClearLogs: TMenuItem;
+    mnuHelp: TMenuItem;
+    mnuLogs: TMenuItem;
+    mnuSep1: TMenuItem;
+    mnuFileUpdate: TMenuItem;
     mnuFileExit: TMenuItem;
     mnuFile: TMenuItem;
     mmMain: TMainMenu;
+    panControlsButtons: TPanel;
+    panLogsStatus: TPanel;
+    psLogs: TPairSplitter;
+    pssLog: TPairSplitterSide;
+    pssError: TPairSplitterSide;
+    panControls: TPanel;
+    panLogs: TPanel;
     psMain: TJSONPropStorage;
+    synLog: TSynEdit;
+    procedure actFileUpdateExecute(Sender: TObject);
+    procedure actHelpAboutExecute(Sender: TObject);
+    procedure actLoggingClearLogsExecute(Sender: TObject);
+    procedure FormClose(Sender: TObject; var CloseAction: TCloseAction);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: boolean);
     procedure FormCreate(Sender: TObject);
   private
     procedure InitPropStorage;
     procedure InitTitle;
     procedure InitShortCuts;
+
+    procedure Output(AMsg: String);
+    procedure Log(AMsg: String);
   public
 
   end;
@@ -37,7 +69,7 @@ const
   cVersionMajor    = 0;
   cVersionMinor    = 1;
   cVersionRevision = 0;
-  cVersionBuild    = 7;
+  cVersionBuild    = 12;
 
 {$R *.lfm}
 
@@ -53,6 +85,32 @@ begin
   InitPropStorage;
   InitTitle;
   InitShortCuts;
+end;
+
+procedure TfrmMain.actFileUpdateExecute(Sender: TObject);
+begin
+  //
+end;
+
+procedure TfrmMain.actHelpAboutExecute(Sender: TObject);
+begin
+  //
+end;
+
+procedure TfrmMain.actLoggingClearLogsExecute(Sender: TObject);
+begin
+  memError.Clear;
+  synLog.Clear;
+end;
+
+procedure TfrmMain.FormClose(Sender: TObject; var CloseAction: TCloseAction);
+begin
+  CloseAction := caFree;
+end;
+
+procedure TfrmMain.FormCloseQuery(Sender: TObject; var CanClose: boolean);
+begin
+  CanClose := True;
 end;
 
 procedure TfrmMain.InitPropStorage;
@@ -81,6 +139,17 @@ begin
 {$IFDEF WINDOWS}
   actFileExit.ShortCut := KeyToShortCut(VK_X, [ssAlt]);
 {$ENDIF}
+end;
+
+procedure TfrmMain.Output(AMsg: String);
+begin
+  //
+end;
+
+procedure TfrmMain.Log(AMsg: String);
+begin
+  memError.Lines.Add(AMsg);
+  Application.ProcessMessages;
 end;
 
 initialization
